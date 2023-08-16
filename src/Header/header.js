42633425eda81bd2f32231528312fd5596e569f5
Mona Elsayed <61633254+Mona-essa@users.noreply.github.com>
2023-08-16 13:53:27 +0300

    Update header.js

--- src/Header/header.js
+++ src/Header/header.js
@@ -13,8 +13,8 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-end">
-            <Nav.Link href="/Home">Home</Nav.Link>
-            <Nav.Link href="/GamingLibrary">Gaming Library</Nav.Link>
+            <Nav.Link href="Home">Home</Nav.Link>
+            <Nav.Link href="./GamingLibrary">Gaming Library</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
