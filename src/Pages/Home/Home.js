85cbd4198e19779519dba6154609388083327fd1
Mona Elsayed <61633254+Mona-essa@users.noreply.github.com>
2023-08-19 23:52:24 +0300

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,5 @@
 import Hero from "../../hero/hero";
-import GamingLibrary from "../../GamingLibrary/GamingLibrary";
-import MostPopular from "../../most popular/most-popular";
+
 
 
 
@@ -8,7 +7,7 @@ const Home = () => {
     return(
         <div>
             <Hero/>
-           <MostPopular />
+          
          
 
         </div>
